fc4dc35a36bb14d19553a06756294d9bb564c27b
newicz <grzesieklukaszewicz@gmail.com>
2025-08-27 16:51:45 +0200

    Frontend ready

---
+++ app/components/trend.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div class="font-bold" :class="[color]">{{ title }}</div>
+
+    <div class="text-2xl font-extrabold text-black dark:text-white mb-2">
+      <USkeleton class="h-8 w-full" v-if="loading" />
+      <div v-else>{{ currency }}</div>
+    </div>
+
+    <div>
+      <USkeleton class="h-6 w-full" v-if="loading" />
+      <div v-else class="flex space-x-1 items-center text-sm">
+        <UIcon
+          :name="icon"
+          class="w-6 h-6"
+          :class="{ green: trandingUp, red: !trandingUp }"
+        />
+        <div class="text-gray-500 dark:text-gray-400">
+          {{ precentageTrend }} vs last period
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: String,
+  amount: Number,
+  lastAmount: Number,
+  color: String,
+  loading: Boolean,
+});
+
+const trandingUp = computed(() => props.amount >= props.lastAmount);
+const icon = computed(() =>
+  trandingUp
+    ? "i-heroicons-arrow-trending-up"
+    : "i-heroicons-arrow-trending-down"
+);
+
+const { currency } = useCurrency(props.amount);
+
+const precentageTrend = computed(() => {
+  if (props.amount === 0 || props.lastAmount === 0) return "n/a %";
+  const bigger = Math.max(props.amount, props.lastAmount);
+  const lower = Math.min(props.amount, props.lastAmount);
+  const ratio = ((bigger - lower) / lower) * 100;
+  return `${Math.ceil(ratio)}%`;
+});
+</script>
+
+<style scoped>
+@reference "../assets/css/main.css";
+.green {
+  @apply text-green-600 dark:text-green-400;
+}
+.red {
+  @apply text-red-600 dark:text-red-400;
+}
+</style>
