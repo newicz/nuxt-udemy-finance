8e7d4b4d1b64355013817e902d7446f9dc45236b
newicz <grzesieklukaszewicz@gmail.com>
2025-08-27 11:23:48 +0200

    initial setup header and nuxt ui

---
+++ app/components/app-header.vue
@@ -0,0 +1,8 @@
+<template>
+  <header class="flex justify-between items-center mt-10">
+    <NuxtLink to="/" class="text-xl font-bold">Finance</NuxtLink>
+    <div>
+      <UAvatar src="https://github.com/benjamincanac.png" alt="Greg" />
+    </div>
+  </header>
+</template>
